23ab58b6c014da4ac4ce9caed30d7d867f964bcb
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-04-27 21:10:06 -0600

    Create basics.py
    Learned basic variables, print command and operators.

---
+++ basics.py
@@ -0,0 +1,35 @@
+# Print a text string
+
+print("Hello World")
+
+# Assigning values to variables
+x = 15
+y = 10
+z = 3.5
+Message = "Hello there"
+
+# Using advanced forms of the print function
+print("Value of x is", x)
+print("Value of y is", y, "\nValue of z is", z)
+
+# Basic operators
+sumValue = x + y
+
+print("Sum of x + y is", sumValue)
+print("Subtraction of x from y is", y - x)
+print(x * y)
+
+k = 2
+print(x ** k) # x to the power of k
+
+expValue = x ** k
+print(expValue)
+
+
+
+
+
+
+
+
+
