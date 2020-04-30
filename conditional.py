e2e5a5efa0d95cf4c30f20eb125722f783310b5d
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-04-29 20:57:57 -0600

    Logical and conditional statements.

---
+++ conditional.py
@@ -0,0 +1,17 @@
+n = 1
+
+if n > 2:
+    print("n is big")
+    print("n is bigger than 2")
+else:
+    if n > 0:
+        print("n is small but positive")
+    else:
+        if n == 0:
+            print("n is zero")
+        else:    
+            print("n is negative")
+
+
+
+print("n is a number")
